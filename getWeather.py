c9a19c56c7fcf60e583bbea2513a348eaa35f348
jonahansmulcrone <jonah.mulcrone@plu.edu>
2024-02-22 11:00:17 -0800

    getWeather.py makes 50+ requests and stores data in csv file

--- getWeather.py
+++ getWeather.py
@@ -21,74 +21,65 @@ This is Home work 2, you are allowed to work in pairs, you can only use chatGPT 
 
 # put your code here
 
-if len(sys.argv) < 3:
+if len(sys.argv) < 2:
     print("This script is going to collect weather data and store in a csv file:")
     print("python "+sys.argv[0] + " \"city-name\" airquality.csv")
 
-CITY = sys.argv[1]
-file_path = sys.argv[2]
+file_path = sys.argv[1]
 
 CURRENT_BASE_URL = f"https://api.openweathermap.org/data/2.5/weather?"
 API_KEY = "5d608d9616a39ff9e56db80e093fa8b1"
 
-current_url = CURRENT_BASE_URL + "q=" + CITY + "&appid=" + API_KEY + "&units=imperial"
+cities = [
+    "New York", "Los Angeles", "Chicago", "Houston", "Phoenix",
+    "Philadelphia", "San Antonio", "San Diego", "Dallas", "San Jose",
+    "Austin", "Jacksonville", "Fort Worth", "Columbus", "Charlotte",
+    "San Francisco", "Indianapolis", "Seattle", "Denver", "Washington",
+    "Boston", "El Paso", "Nashville", "Detroit", "Oklahoma City",
+    "Portland", "Las Vegas", "Memphis", "Louisville", "Baltimore",
+    "Milwaukee", "Albuquerque", "Tucson", "Fresno", "Mesa",
+    "Sacramento", "Atlanta", "Kansas City", "Colorado Springs", "Miami",
+    "Raleigh", "Omaha", "Long Beach", "Virginia Beach", "Oakland",
+    "Minneapolis", "Tampa", "Tulsa", "Arlington"
+]
 
-current_response = requests.get(current_url).json()
-print("Response:", current_response)
+for i in range(len(cities)):
 
-current_temp = round(current_response['main']['temp'], 1)
-current_desc = current_response['weather'][0]['description']
-current_humidity = current_response['main']['humidity']
-current_wind_speed = current_response['wind']['speed']
-current_wind_deg = current_response['wind']['deg']
-current_visibility = current_response['visibility']
-current_date = datetime.now()
+    current_url = CURRENT_BASE_URL + "q=" + cities[i] + "&appid=" + API_KEY + "&units=imperial"
+    current_response = requests.get(current_url).json()
+    print("Response:", current_response)
 
-formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    current_temp = round(current_response['main']['temp'], 1)
+    current_desc = current_response['weather'][0]['description']
+    current_humidity = current_response['main']['humidity']
+    current_wind_speed = current_response['wind']['speed']
+    current_wind_deg = current_response['wind']['deg']
+    current_visibility = current_response['visibility']
+    current_date = datetime.now()
 
-print(f"The current temperature in Seattle on {formatted_date} is {current_temp}F with {current_desc}. The humidity is {current_humidity}%")
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
 
-FORECAST_BASE_URL = f"https://api.openweathermap.org/data/2.5/forecast?"
-forecast_url = FORECAST_BASE_URL + "q=" + CITY + "&appid=" + API_KEY + "&units=imperial"
+    print(f"The current temperature in {cities[i]} on {formatted_date} is {current_temp}F with {current_desc}. The humidity is {current_humidity}%")
 
-forecast_response = requests.get(forecast_url).json()
-print("Forecast Response:", forecast_response)
+    current_weather = {
+        "City": cities[i],
+        "Time": current_date,
+        "Temperature" : int(current_temp),
+        "Humidity": int(current_humidity),
+        "Wind Speed": int(current_wind_speed),
+        "Wind Deg": int(current_wind_deg),
+        "Visibility": int(current_visibility)
+    }
 
-forecast_temp = round(forecast_response['list'][0]['main']['temp'], 1)
-forecast_desc = forecast_response['list'][0]['weather'][0]['description']
-forecast_humidity = forecast_response['list'][0]['main']['humidity']
-forecast_wind_speed = forecast_response['list'][0]['wind']['speed']
-forecast_wind_deg = forecast_response['list'][0]['wind']['deg']
-forecast_visibility = forecast_response['list'][0]['visibility']
-forecast_date = forecast_response['list'][0]['dt_txt']
 
-print(f"The temperature forecast for {forecast_date} is {forecast_temp}F with {forecast_desc}. The humidity will be {forecast_humidity}%.")
-
-current_weather = {
-    "City": CITY,
-    "Time": current_date,
-    "Temperature" : int(current_temp),
-    "Humidity": int(current_humidity),
-    "Wind Speed": int(current_wind_speed),
-    "Wind Deg": int(current_wind_deg),
-    "Visibility": int(current_visibility)
-}
-
-forecast_weather = {
-    "City": CITY,
-    "Time": forecast_date,
-    "Temperature": int(forecast_temp),
-    "Humidity": int(forecast_humidity),
-    "Wind Speed": int(forecast_wind_speed),
-    "Wind Deg": int(forecast_wind_deg),
-    "Visibility": int(forecast_visibility)
-}
-
-with open(file_path, mode='w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(['Weather Element', 'Current', 'Forecast', '% Change'])
-    for element in current_weather.keys():
-        if (type(current_weather[element]) == int):
-            writer.writerow([element, current_weather[element], forecast_weather[element], round((forecast_weather[element] - current_weather[element]) / current_weather[element] * 100, 5)])
+    with open(file_path, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        
+        if i < 1:
+            writer.writerow(['City', 'Time', 'Temperature', 'Humidity', 'Wind Speed', 'Wing Deg', 'Visibility'])
         else:
-            writer.writerow([element, current_weather[element], forecast_weather[element], "N/A"])
+            writer.writerow([current_weather["City"], current_weather["Time"], current_weather["Temperature"], current_weather["Humidity"], current_weather["Wind Speed"], current_weather['Wind Deg'], current_weather["Visibility"]])
+
+
+
+
