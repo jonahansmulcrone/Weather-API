c0abe98456c61f3c85daa1a457e93b39a3605b12
Emmanuel <obikwelu@plu.edu>
2024-02-22 23:17:59 -0800

    Finish the  database

--- HW_03.py
+++ HW_03.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import sqlite3
+import csv
 
 """
 In this homework, you are allowed to work in pairs, please get your API ready and use it in this code.
@@ -28,24 +29,41 @@ class saving2Database():
         output would be the path of your sqlite3 database and the data will be saved there
         """
         # set up the mysqlite3 db connection 
-        con = sqlite3.connect('weather.db')
+        con = sqlite3.connect(output)
         cur = con.cursor()
-        keyWord = "Temperature"
-        recordLimit = 10
+
 
         # initialize the table for the database
         self.initTable()
-        
+        for row in cur.execute('SELECT * FROM weather WHERE city = ?', (keyWord,)):
+            print(row)
+        con.commit()  # save (commit) the changes
+        con.close()
         pass
     
     def initTable(self):
         """
         Discuss and think about what kind of table you want to create for your database, like the weather, the location, and others? also think about the size of the column, you may need to find out the maximum length for each feature
         """
+
+        con = sqlite3.connect('weather.db')
+        cur = con.cursor()
+        cur.execute('''CREATE TABLE Weather (city text, time text,  temperature text, humidity text, wind_speed text, wind_deg text, visibility text)''')
+
+
+        with open('weather_data.csv', mode='r') as file:
+            reader = csv.reader(file)
+            next(reader)
+            for row in reader:
+                cur.execute("INSERT INTO Weather VALUES (?, ?, ?, ?, ?, ?, ?)",(row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
+        con.commit()
+        con.close()
         pass
 
  
 
 # example code that you can run and test 
-# for testing you can use 1000, and the topic can be different 
-myt = saving2Database(["sunny","cloudy"], 1000, "weatherDBtop1000.db") 
+# for testing you can use 1000, and the topic can be different
+key = sys.argv[1]
+myt = saving2Database(key, 1000, "weather.db")
+#print(myt)
